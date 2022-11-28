8d5f4fb7479759b298599bd22a464f43422789fb
sambeck87 <sandro_mipb@hotmail.com>
2022-11-28 13:59:37 -0600

    A HTML file was created A javaScript file was created A Event listener was created to save the values

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const buttonAdd = document.getElementById('add');
+let booksData = [];
+check = localStorage.getItem('books');
+if(check != null){
+booksData = JSON.parse(check);
+}
 
 buttonAdd.addEventListener('click', () => {
-  const booksData = [];
-  const bookData = [title.value, author.value];
-  booksData.push[bookData];
+  const bookTitle = title.value;
+  const bookAuthor = author.value;
+  const bookData = [bookTitle, bookAuthor];
+  booksData.push(bookData);
+  const allData = JSON.stringify(booksData);
+  localStorage.setItem('books', allData);
+  location.reload()
 })
+
+let getBooks = localStorage.getItem('books');
+getBooks = JSON.parse(getBooks);
+console.log(getBooks);
+
+
+if (getBooks.length > 0) {
+  const booksSection = document.getElementById('books');
+  let book=''
+  for (let i = 0; i < getBooks.length; i++) {
+    book += `<div id="book${i}"><p>${getBooks[i][0]}</p>
+    <p>${getBooks[i][1]}</p>
+    <button id="remove_btn" value="${i}>Remove</button><br>
+  <hr>
+  </div>`
+  }
+  console.log(book);
+  booksSection.innerHTML = `${book}`
+}
+
