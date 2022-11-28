e98ebb52b077001acd1b8d65b42f257f82d340b3
sambeck87 <sandro_mipb@hotmail.com>
2022-11-28 12:02:27 -0600

    A HTML file was created A javaScript file was created A Event listener was created to save the values

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const buttonAdd = document.getElementById('add');
+
+buttonAdd.addEventListener('click', () => {
+  const booksData = [];
+  const bookData = [title.value, author.value];
+  booksData.push[bookData];
+})
