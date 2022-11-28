3fec79b3a087e9fc7aee0d8c26da9653f4933981
Roodjino Cherilus <cherilusjcr@gmail.com>
2022-11-28 21:55:23 +0000

    fix linter error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const title = document.getElementById('title');
 const author = document.getElementById('author');
 const buttonAdd = document.getElementById('add');
 let booksData = [];
-check = localStorage.getItem('books');
+const check = localStorage.getItem('books');
 if (check != null) {
   booksData = JSON.parse(check);
 }
@@ -14,36 +14,33 @@ buttonAdd.addEventListener('click', () => {
   booksData.push(bookData);
   const allData = JSON.stringify(booksData);
   localStorage.setItem('books', allData);
-  location.reload()
-})
+  location.reload();
+});
 
 let getBooks = localStorage.getItem('books');
 getBooks = JSON.parse(getBooks);
 
-
 if (getBooks.length > 0) {
   const booksSection = document.getElementById('books');
-  let book = ''
-  for (let i = 0; i < getBooks.length; i++) {
+  let book = '';
+  for (let i = 0; i < getBooks.length; i+=1) {
     book += `<div id="book${i}"><p>${getBooks[i][0]}</p>
     <p>${getBooks[i][1]}</p>
     <button class="button" value=${i}>Remove</button><br>
   <hr>
-  </div>`
+  </div>`;
   }
-  booksSection.innerHTML = `${book}`
+  booksSection.innerHTML = `${book}`;
 }
 
 const deleteBtn = document.querySelectorAll('.button');
 
-deleteBtn.forEach(deleteBtn => {
-  deleteBtn.addEventListener("click", () => {
-    let index = deleteBtn.value;
+deleteBtn.forEach((deleteBtn) => {
+  deleteBtn.addEventListener('click', () => {
+    const index = deleteBtn.value;
     getBooks.splice(index, 1);
     getBooks = JSON.stringify(getBooks);
     localStorage.setItem('books', getBooks);
-    location.reload()
+    location.reload();
   });
 });
-
-
