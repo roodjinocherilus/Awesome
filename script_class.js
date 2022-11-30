c074ed02f829a56450e35b3e64986a35f7420cab
Roodjino Cherilus <cherilusjcr@gmail.com>
2022-11-30 20:07:15 +0000

    fix linters error and the default state of the web app

--- script_class.js
+++ script_class.js
@@ -98,51 +98,50 @@ deleteBtn.forEach((btn) => {
   });
 });
 
-
-document.getElementById('date').innerHTML=Date();
+document.getElementById('date').innerHTML = Date();
 
 const list = document.getElementById('list-books');
 const subtitle = document.getElementById('subtitle');
 const form = document.getElementById('add-books');
 const contact = document.getElementById('contact');
 
-const btn_list = document.getElementById('listLink');
-const btn_addNew = document.getElementById('addLink');
-const btn_Contact = document.getElementById('contactLink');
+const btnList = document.getElementById('listLink');
+const btnAddNew = document.getElementById('addLink');
+const btnContact = document.getElementById('contactLink');
 
 function List() {
-  list.classList.remove('hide')
-  subtitle.classList.add('hide')
-  form.classList.add('hide')
-  contact.classList.add('hide')
-  btn_list.classList.add('blue')
-  btn_list.classList.remove('link_nav')
-  btn_addNew.classList.add('link_nav')
-  btn_Contact.classList.add('link_nav')
+  list.classList.remove('hide');
+  subtitle.classList.add('hide');
+  form.classList.add('hide');
+  contact.classList.add('hide');
+  btnList.classList.add('blue');
+  btnList.classList.remove('link_nav');
+  btnAddNew.classList.add('link_nav');
+  btnContact.classList.add('link_nav');
 }
 function AddNew() {
-  list.classList.add('hide')
-  subtitle.classList.remove('hide')
-  form.classList.remove('hide')
-  contact.classList.add('hide')
-  btn_addNew.classList.add('blue')
-  btn_addNew.classList.remove('link_nav')
-  btn_list.classList.add('link_nav')
-  btn_Contact.classList.add('link_nav')
+  list.classList.add('hide');
+  subtitle.classList.remove('hide');
+  form.classList.remove('hide');
+  contact.classList.add('hide');
+  btnAddNew.classList.add('blue');
+  btnAddNew.classList.remove('link_nav');
+  btnList.classList.add('link_nav');
+  btnContact.classList.add('link_nav');
 }
 function Contact() {
-  list.classList.add('hide')
-  subtitle.classList.add('hide')
-  form.classList.add('hide')
-  contact.classList.remove('hide')
-  btn_Contact.classList.add('blue')
-  btn_Contact.classList.remove('link_nav')
-  btn_list.classList.add('link_nav')
-  btn_addNew.classList.add('link_nav')
+  list.classList.add('hide');
+  subtitle.classList.add('hide');
+  form.classList.add('hide');
+  contact.classList.remove('hide');
+  btnContact.classList.add('blue');
+  btnContact.classList.remove('link_nav');
+  btnList.classList.add('link_nav');
+  btnAddNew.classList.add('link_nav');
 }
 
-btn_list.addEventListener('click', List);
+btnList.addEventListener('click', List);
 
-btn_addNew.addEventListener('click', AddNew);
+btnAddNew.addEventListener('click', AddNew);
 
-btn_Contact.addEventListener('click', Contact);
+btnContact.addEventListener('click', Contact);
